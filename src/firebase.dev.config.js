30144eba3a49e457c32e72b10c88b1039eef6025
Mushfiqur Rahman <realmrahman.19@gmail.com>
2024-04-17 20:04:06 -0400

    Added firebase config instructions

---
+++ src/firebase.dev.config.js
@@ -0,0 +1,14 @@
+/*
+Copy and rename the file to firebase.config.js
+and fill in the values with your firebase project configuration
+*/
+
+export default {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
+}
--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,8 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 // Import the functions you need from the SDKs you need
 import { getAnalytics } from 'firebase/analytics'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {}
+import firebaseConfig from './firebase.config.js'
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig)
